9227c6e238bb04a8083f1edc46361123be72b70b
Nainoa <nainoa.villet@gmail.com>
2022-05-09 10:05:25 +0200

    create project

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -12,7 +12,7 @@
         </ion-toolbar>
       </ion-header>
     
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer name="Ionic_Project" />
     </ion-content>
   </ion-page>
 </template>
